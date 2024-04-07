d69886b08b3900d89ec145cd003e968a97565faf
tesis <pabloalonsoxx@gmail.com>
2024-04-07 14:21:18 -0300

    Clean testing.mk

--- testing.mk
+++ testing.mk
@@ -13,16 +13,16 @@ EXE	:= $(shell cabal exec whereis compiladores2023 | awk '{print $$2};')
 VM	:= ./vm/macc
 
 EXTRAFLAGS	:=
-EXTRAFLAGS	+= --optimize
+# EXTRAFLAGS	+= --optimize
 
 # Las reglas a chequear. Se puede deshabilitar toda una familia de tests
 # comentando una de estas líneas.
-# CHECK	+= $(patsubst %,%.check_eval,$(TESTS))
-# CHECK	+= $(patsubst %,%.check_cek,$(TESTS))
-# CHECK	+= $(patsubst %,%.check_bc32_h,$(TESTS))
-# CHECK	+= $(patsubst %,%.check_bc32,$(TESTS))
-# CHECK	+= $(patsubst %,%.check_eval_opt,$(TESTS))
-# CHECK	+= $(patsubst %,%.check_opt,$(TESTS))
+CHECK	+= $(patsubst %,%.check_eval,$(TESTS))
+CHECK	+= $(patsubst %,%.check_cek,$(TESTS))
+CHECK	+= $(patsubst %,%.check_bc32_h,$(TESTS))
+CHECK	+= $(patsubst %,%.check_bc32,$(TESTS))
+#CHECK	+= $(patsubst %,%.check_eval_opt,$(TESTS))
+#CHECK	+= $(patsubst %,%.check_opt,$(TESTS))
 CHECK	+= $(patsubst %,%.check_cc,$(TESTS))
 
 # Ejemplo: así se puede apagar un test en particular.
